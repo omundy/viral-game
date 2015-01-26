ed28ae224a2e825ab4382af642d924370068467f
Owen Mundy <omundy@gmail.com>
2015-01-26 14:56:02 -0500

    added objs

--- www/assets/js/objects.js
+++ www/assets/js/objects.js
@@ -17,13 +17,32 @@ var instagram_camera_roll = {
 			'troll':0
 		}
 		'img': 'instagram/dikpic.jpg',
+		'unlocked':false,
 		'add_score':add_score()
 	},
 	
 	
-	
+	dikpic.jpg (+1 martyr)
+lilb.jpg (+1troll)
+nails.jpg (basic, -1 in every category)
+nude.jpg (+1 Camgirl)
+scenequeen.jpg (+1 camgirl)
+weenie.jpg (+1 troll)
+fedora.jpg (+1 troll)
+arsenal.jpg (+1 martyr)
+dylan.jpg (+1 martyr)
+cats.jpg (+1 Camgirl)
+moloshe2.jpg (+1 camgirl)
+moloshe1.jpg (+1 troll)
 	
 	
 	
 
 }
+
+
+
+
+
+// one tags object
+// 18
