cbedba07628856fd003134469be104ccbbb52771
Owen Mundy <omundy@gmail.com>
2015-02-02 14:14:50 -0500

    This adds some new images

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -39,3 +39,27 @@ function print_obj (obj){
 
 	
 }
+
+
+var screenW,screenH;
+
+window.addEventListener('resize', setWindowSize);
+
+function setWindowSize() {
+	if (typeof (window.innerWidth) == 'number') {
+		screenW = window.innerWidth;
+		screenH = window.innerHeight;
+	} else {
+		if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+			screenW = document.documentElement.clientWidth;
+			screenH = document.documentElement.clientHeight;
+		} else {
+			if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+				screenW = document.body.clientWidth;
+				screenH = document.body.clientHeight;
+			}
+		}
+	}
+	console.log('screen: '+screenW+','+screenH);
+}
+	
