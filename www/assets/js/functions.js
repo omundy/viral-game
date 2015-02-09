1feb1fd6a60244fc5967386c4e9b6f42ddee4223
Owen Mundy <omundy@gmail.com>
2015-02-09 18:15:31 -0500

    Adds min and max frame to key commands

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -1,24 +1,4 @@
 
-function create_buttons(obj){
-	
-	$.each( obj.buttons, function( key, value ) {
-		// make button
-		if (value.locked == true){
-			var btn = '<img src ="assets/img/'+ obj.meta.path + value.btn_img +'">'
-		} else if (value.disabled == true){
-			var btn = '<img src ="assets/img/'+ obj.meta.path + value.btn_img +'">'
-		} else {
-			var btn = '<img src ="assets/img/'+ obj.meta.path + value.btn_img +'">'
-		}
-		$('body').append(btn)
-	});
-	
-	
-	for (var key in obj.buttons) {
-		
-		$('body').append(obj.buttons)
-	}
-}
 
 
 
