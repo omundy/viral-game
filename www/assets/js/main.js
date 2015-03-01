11e04b915d3bd0bd7dbb080c29883a6cb2340c42
l3nanw11 <l3na.nw@gmail.com>
2015-03-01 07:57:21 -0500

    all links are correct
    new imags

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -141,23 +141,42 @@ $(document).ready(function(){
 	
 	
 	
-	/* GLOBAL BUTTONS */
+/* GLOBAL BUTTONS */
 	
 	$('.home_button')
 		.on('click',function(){ scene_control('home',0)})
 		.mouseover(function(){ this.src = 'assets/img/instacam/bakhov.png' })
 		.mouseout(function(){ this.src = 'assets/img/instacam/bak.png' });
-		
 	$('.back_button')
-		.on('click',function(){ scene_control(current_scene.scene,current_scene.frame-1) })
+		.on('click',function(){ scene_control(current_scene.scene,0) })
 		.mouseover(function(){ this.src = 'assets/img/instacam/bakhov.png' })
 		.mouseout(function(){ this.src = 'assets/img/instacam/bak.png' });
 	$('.next_button')
-		.on('click',function(){ scene_control(current_scene.scene,current_scene.frame+1) })
+		.on('click',function(){ scene_control(current_scene.scene,current_scene.frame +1) })
 		.mouseover(function(){ this.src = 'assets/img/instacam/nexthov.png' })
 		.mouseout(function(){ this.src = 'assets/img/instacam/next.png' });
 	
+	/* HOMESCREEN BUTTONS */
 	
+	$('.instacam_button')
+		.on('click',function(){ scene_control('instacam',0)})
+		.mouseover(function(){ this.src = 'assets/img/home/instacam.gif' })
+		.mouseout(function(){ this.src = 'assets/img/home/instacam.gif' });
+	$('.metube_button')
+		.on('click',function(){ scene_control('metube', 0) })
+		.mouseover(function(){ this.src = 'assets/img/home/metube.gif' })
+		.mouseout(function(){ this.src = 'assets/img/home/metube.gif' });
+	$('.dumblr_button')
+		.on('click',function(){ scene_control('dumblr', 0) })
+		.mouseover(function(){ this.src = 'assets/img/home/dumblr.gif' })
+		.mouseout(function(){ this.src = 'assets/img/home/dumblr.gif' });
+	$('.cinder_button')
+		.on('click',function(){ scene_control('cinder', 0) })
+		.mouseover(function(){ this.src = 'assets/img/home/cinder.gif' })
+		.mouseout(function(){ this.src = 'assets/img/home/cinder.gif' });
+	
+	
+
 	/* INSTACAM BUTTONS */
 	
 	$('#instacam_home_selselfie_btn')
@@ -165,12 +184,53 @@ $(document).ready(function(){
 		.mouseover(function(){ this.src = 'assets/img/instacam/home/selselfiehov.png' })
 		.mouseout(function(){ this.src = 'assets/img/instacam/home/selselfie.png' });
 	$('#instacam_home_selfood_btn')
-		.on('click',function(){ scene_control('instacam',2) })
+		.on('click',function(){ scene_control('instacam',4) })
 		.mouseover(function(){ this.src = 'assets/img/instacam/home/selfoodhov.png' })
 		.mouseout(function(){ this.src = 'assets/img/instacam/home/selfood.png' });
-	$('#instacam_home_selcam_btn').on('click',function(){ scene_control('instacam',3) })
+	$('#instacam_home_selcam_btn').on('click',function(){ scene_control('instacam',6) })
 		.mouseover(function(){ this.src = 'assets/img/instacam/home/selcamhov.png' })
 		.mouseout(function(){ this.src = 'assets/img/instacam/home/selcam.png'});
+	$('.instacam_totags').on('click',function(){ scene_control('instacam', 7) })
+		.mouseover(function(){ this.src = 'assets/img/instacam/nexthov.png' })
+		.mouseout(function(){ this.src = 'assets/img/instacam/next.png' });
+		
+	/*METUBE BUTTONS */
+	
+	$('.metube_mv')
+		.on('click',function(){ scene_control('metube',1) });
+	$('.metube_vlog')
+		.on('click',function(){ scene_control('metube',2) });
+	$('.metube_shock').on('click',function(){ scene_control('metube',3) });
+	
+	/*DUMBLR BUTTONS */
+	
+	$('.reblog')
+		.on('click',function(){ scene_control('dumblr',2) });
+	$('.entry')
+		.on('click',function(){ scene_control('dumblr',1) });
+	$('.diary')
+		.on('click',function(){ scene_control('dumblr',1) });
+	$('.manifesto')
+		.on('click',function(){ scene_control('dumblr',1) });
+	$('.entry')
+		.on('click',function(){ scene_control('dumblr',1) });
+		
+	/*CINDER BUTTONS */
+	
+	$('.pass')
+		.on('click',function(){ scene_control(current_scene.scene,current_scene.frame +1) });
+	$('.lastpass')
+		.on('click',function(){ scene_control('cinder', 0) });
+	$('.date_lilb')
+		.on('click',function(){ scene_control('cinder',1) });
+	$('.date_bro')
+		.on('click',function(){ scene_control('cinder',1) });
+	$('.date_scene')
+		.on('click',function(){ scene_control('cinder',1) });
+	$('.date_elliot')
+		.on('click',function(){ scene_control('cinder',1) });
+
+
 	
 	
 	
